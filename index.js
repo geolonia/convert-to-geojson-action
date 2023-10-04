b0fb2ceff2137ae16062a79dfa5e0f74dc8f1721
naoki <n.globe.us@gmail.com>
2023-10-04 11:07:14 +0900

    Excel/CSV から GeoJSON に変換

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+
 const core = require('@actions/core');
 const { excel2csv } = require('./excel2csv');
-const { writeFile } = require('fs/promises');
+const { writeFile, readFile } = require('fs/promises');
 const klaw = require('klaw');
-const { basename, dirname, join } = require('path');
+const { join } = require('path');
+const csv2geojson = require('csv2geojson');
 const inputDir = core.getInput('input_dir');
 
 const main = async () => {
@@ -11,26 +13,36 @@ const main = async () => {
   const workspace = process.env.GITHUB_WORKSPACE;
 
   for await (const file of klaw(join(workspace, inputDir), { depthLimit: -1 })) {
+    let csvData;
 
     if (file.path.endsWith(".xlsx")) {
       const excelPath = file.path;
-      const csvPath = join(dirname(excelPath), `${basename(excelPath, ".xlsx")}.csv`);
-
-      promises.push((async () => {
-        try {
-          const csv = await excel2csv(excelPath);
-          await writeFile(csvPath, csv);
-        } catch (err) {
-
-          core.setFailed(`Error: Excel ファイル ${excelPath} を CSV に変換できませんでした。`);
-
-          if (err.message === "FILE_ENDED") {
-            core.setFailed("データが空になっているか、Excel ファイルが破損している可能性があります。");
-          }
-
-          throw err;
+      try {
+        csvData = await excel2csv(excelPath);
+      } catch (err) {
+        core.setFailed(`Error: Excel ファイル ${excelPath} を GeoJSON に変換できませんでした。`);
+        if (err.message === "FILE_ENDED") {
+          core.setFailed("データが空になっているか、Excel ファイルが破損している可能性があります。");
         }
-      })());
+        throw err;
+      }
+    } else if (file.path.endsWith(".csv")) {
+      csvData = await readFile(file.path, 'utf-8');
+    }
+
+    if (csvData) {
+      const geoJsonPath = file.path.replace(/.csv$|.xlsx$/, '.geojson');
+      try {
+        const geojson = csv2geojson.csv2geojson(csvData, {
+          latfield: 'lat',
+          lonfield: 'lng',
+          delimiter: ','
+        });
+        await writeFile(geoJsonPath, JSON.stringify(geojson));
+      } catch (err) {
+        core.setFailed(`Error: CSV データ ${file.path} を GeoJSON に変換できませんでした。`);
+        throw err;
+      }
     }
   }
 
